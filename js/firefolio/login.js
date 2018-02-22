9a233783c82dd735f75d980e837f04709355ab59
Unknown <b6010572@my.shu.ac.uk>
2018-02-22 12:45:29 +0000

    Prevented default form event

--- js/firefolio/login.js
+++ js/firefolio/login.js
@@ -1,5 +1,13 @@
-$('document').ready({
+$('document').ready(function () {
   var login = {
+    form: $('#form'),
+    request: {},
+    attempt: function () {
 
-  }
+    }
+  };
+
+  login.form.on('submit', function (event) {
+    event.preventDefault();
+  });
 });
