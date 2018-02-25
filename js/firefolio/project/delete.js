c32d84df971bf283e2dafb0962d2962988e2f460
Unknown <b6010572@my.shu.ac.uk>
2018-02-25 17:31:13 +0000

    Missing property in object

--- js/firefolio/project/delete.js
+++ js/firefolio/project/delete.js
@@ -2,6 +2,7 @@ $('document').ready(function () {
   // Can't use 'delete' as that's a reserved keyword
   var del = {
     button: $('#delete'),
+    url: 'projects/delete',
     attempt: function (checkboxes, uris) {
       var projects_to_delete = [];
 
@@ -11,7 +12,23 @@ $('document').ready(function () {
         }
       }
 
-      console.log(projects_to_delete);
+      var data = {
+        projects: JSON.stringify(projects_to_delete);
+      };
+
+      request = $.post(
+        del.url,
+        data,
+        'JSON'
+      );
+
+      request.done(function (message) {
+
+      });
+
+      request.fail(function (message) {
+        console.console.error(message);
+      });
     }
   };
 
