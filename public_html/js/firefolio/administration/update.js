17286683ff9a9954402e9adf0d1b98769bc29308
Unknown <b6010572@my.shu.ac.uk>
2018-03-02 14:35:19 +0000

    Added event handlers for administration forms

--- public_html/js/firefolio/administration/update.js
+++ public_html/js/firefolio/administration/update.js
@@ -1,7 +1,29 @@
 $('document').ready(function () {
   var update = {
-    url: $('#base_url')
+    url: $('#base_url'),
+    attempt: {
+      username: function () {
+        console.log('username');
+      },
+      password: function () {
+        console.log('password');
+      }
+    }
   };
 
-  console.log('upd');
+  if ($('#update-username') != null) {
+    $('#update-username').submit(function (event) {
+      event.preventDefault();
+
+      update.attempt.username();
+    });
+  }
+
+  if ($('#update-password') != null) {
+    $('#update-password').submit(function (event) {
+      event.preventDefault();
+
+      update.attempt.password();
+    });
+  }
 });
