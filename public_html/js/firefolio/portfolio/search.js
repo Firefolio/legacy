65980d0e5607890324ec82c73a961ce8659da2cb
Unknown <b6010572@my.shu.ac.uk>
2018-03-08 09:00:06 +0000

    Added code from other search script

--- public_html/js/firefolio/portfolio/search.js
+++ public_html/js/firefolio/portfolio/search.js
@@ -1,5 +1,42 @@
 $('document').ready(function () {
   var search = {
-    url: ''
+    bar: $('#search'),
+    list: $('#projects'),
+    url: $('#base-url').val() + 'index.php/firefolio/projects/search',
+    attempt: function () {
+      var data = {
+        query: search.bar.val()
+      };
+      data[$('#csrf').attr('name')] = $('#csrf').val();
+
+      var request = $.post(
+        search.url,
+        data,
+        'JSON'
+      );
+
+      request.done(function (response) {
+        response = JSON.parse(response);
+
+        // Remember to always regenerate the anti-CSRF hash
+        $('#csrf').val(response.hash);
+
+        if (response.success) {
+          search.list.html(response.html);
+        } else {
+          console.error(response.message);
+        }
+      });
+
+      request.fail(function (message) {
+        console.error(message);
+      });
+    }
   };
+
+  if (search.bar != null) {
+    search.bar.on('input', function () {
+      search.attempt();
+    });
+  }
 });
