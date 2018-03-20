4bd729f0e8aa03d236d3b2fa5ca9c71e20eb912e
Unknown <b6010572@my.shu.ac.uk>
2018-03-20 21:49:06 +0000

    Added a model for programming languages

---
+++ application/models/Language_model.php
@@ -0,0 +1,13 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+/**
+ * Used for managing the list of programming languages
+ */
+class Language_model extends CI_Model
+{
+  function __construct()
+  {
+    # code...
+  }
+}
