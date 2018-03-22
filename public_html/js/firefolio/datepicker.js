4ec58e5469230ff808270a82c218a6b2bd500047
Unknown <b6010572@my.shu.ac.uk>
2018-03-22 21:50:26 +0000

    Added a link to log in on the front page

--- public_html/js/firefolio/datepicker.js
+++ public_html/js/firefolio/datepicker.js
@@ -2,6 +2,7 @@ $('document').ready(function () {
   $('#datepicker').datepicker({
     showOn: 'focus',
     changeMonth: true,
-    changeYear: true,dateFormat: 'mm-dd-yy'
+    changeYear: true,
+    dateFormat: 'mm-dd-yy'
  });
 });
