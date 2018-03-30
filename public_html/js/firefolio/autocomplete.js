bf64d9d06b4f0540d86d9586d19b72f8b0dc7d65
Unknown <b6010572@my.shu.ac.uk>
2018-03-30 14:02:40 +0100

    Added basic autocomplete

--- public_html/js/firefolio/autocomplete.js
+++ public_html/js/firefolio/autocomplete.js
@@ -1,3 +1,31 @@
 $('document').ready(function () {
+  var languages = [
+    "ActionScript",
+    "AppleScript",
+    "Asp",
+    "BASIC",
+    "C",
+    "C++",
+    "Clojure",
+    "COBOL",
+    "ColdFusion",
+    "Erlang",
+    "Fortran",
+    "Groovy",
+    "Haskell",
+    "Java",
+    "JavaScript",
+    "Lisp",
+    "Perl",
+    "PHP",
+    "Python",
+    "Ruby",
+    "Scala",
+    "Scheme"
+  ];
 
+  // Autocomplete the languages field based on the values in languages
+  $('#autocomplete').autocomplete({
+    source: languages
+  });
 });
