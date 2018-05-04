6a419503679827f2edf4141ade3c13e2dca20be8
Unknown <b6010572@my.shu.ac.uk>
2018-05-04 16:55:11 +0000

    Backend search now only enables when the DOM is ready

--- public_html/js/firefolio/project/search.js
+++ public_html/js/firefolio/project/search.js
@@ -2,6 +2,9 @@ $('document').ready(function () {
   var url = $('#base-url').val() + $('#index-page').val() + '/backend/projects/search';
   var limit = 64; // Milliseconds
 
+  // Make sure that the search bar is ready
+  $('#search').removeAttr('disabled');
+
   $('#search').on('input', debounce(function () {
     ajax.request.html(
       $('#search').val(),
