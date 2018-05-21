65ce9e0a6a1299d1eca11d2b2b5a2c7bd1ab5b95
Unknown <b6010572@my.shu.ac.uk>
2018-05-21 13:23:45 +0000

    New screenshots can't be deleted

--- public_html/js/firefolio/screenshot/delete.js
+++ public_html/js/firefolio/screenshot/delete.js
@@ -9,13 +9,11 @@ screenshots.delete = function (id) {
   ajax.request.data(data, url);
 }
 
-$('document').ready(function () {
-  $('.delete-screenshot').click(function (event) {
-    event.preventDefault();
+$('.screenshot').on('click', '.delete-screenshot', function (event) {
+  event.preventDefault();
 
-    // Remove that screenshot from the database via an AJAX request
-    screenshots.delete($(this).data('id'));
-    // Hide the input in the DOM
-    console.log($(this).parents());
-  });
+  // Remove that screenshot from the database via an AJAX request
+  screenshots.delete($(this).parents('.screenshot').data('id'));
+  // Remove that element from the DOM
+  $(this).parents('.screenshot').remove();
 });
